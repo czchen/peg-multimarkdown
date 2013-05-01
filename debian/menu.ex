e21b6cd76c8aae4f30a42c2897a554a03f945204
ChangZhuo Chen <czchen@gmail.com>
2013-05-01 20:04:36 +0800

    Remove debian/*.ex

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(multimarkdown):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="multimarkdown" command="/usr/bin/multimarkdown"
--- debian/multimarkdown.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the multimarkdown package
-#
-0 4	* * *	root	[ -x /usr/bin/multimarkdown_maintenance ] && /usr/bin/multimarkdown_maintenance
